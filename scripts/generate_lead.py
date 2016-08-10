a42805233322562b39a246e02265dcbe21ff2953
Prashant Khanduri <prashant.khanduri@gmail.com>
2016-08-10 11:24:30 -0700

    leads folder

--- scripts/generate_lead.py
+++ scripts/generate_lead.py
@@ -1,5 +1,6 @@
 import sys
 import copy
+from os import walk
 
 
 TABS = {
@@ -20,8 +21,8 @@ LEAD_TEMPLATE = [
 ]
 
 
-file_to_open = sys.argv[1]
-file_to_write = open('{0}_LEAD'.format(file_to_open), 'w')
+songs_folder = sys.argv[1]
+leads_folder = 'leads'
 
 
 def clean_file(file_to_open, songs_lead):
@@ -56,40 +57,48 @@ def add_song_lead(song_lead, notes):
             song_lead[index].append('-- ')
 
 
-song_lead_template = [[],[],[],[],[],[],]
-song_leads = []
-for line in clean_file(file_to_open, song_leads):
-    chunks = line.split()
-    song_lead = copy.deepcopy(song_lead_template)
-    for chunk in chunks:
-        notes = chunk.split('-')
+song_files = []
+for (dirpath, dirnames, filenames) in walk(songs_folder):
+    song_files.extend(filenames)
+    break
 
-        operators = {n[0]: n[1:] for n in notes}
+for song_file in song_files:
+    file_to_open = "/".join([songs_folder, song_file])
+    file_to_write = open("/".join([leads_folder, song_file]), 'w')
 
-        if len(operators) == 1:
-            operator = operators.keys()[0]
-            if operator not in TABS:
-                if operator == '*':
-                    count = int(operators.values()[0])
-                    for _ in xrange(count):
-                        song_lead = copy.deepcopy(song_lead)
-                        song_leads.append(song_lead)
-                    song_lead = copy.deepcopy(song_lead_template)
-                if operator == '.':
-                    for index in xrange(len(LEAD_TEMPLATE)):
-                        song_lead[index].append('. ')
+    song_lead_template = [[],[],[],[],[],[],]
+    song_leads = []
+    for line in clean_file(file_to_open, song_leads):
+        chunks = line.split()
+        song_lead = copy.deepcopy(song_lead_template)
+        for chunk in chunks:
+            notes = chunk.split('-')
 
-            else:
+            operators = {n[0]: n[1:] for n in notes}
+
+            if len(operators) == 1:
+                operator = operators.keys()[0]
+                if operator not in TABS:
+                    if operator == '*':
+                        count = int(operators.values()[0])
+                        for _ in xrange(count):
+                            song_lead = copy.deepcopy(song_lead)
+                            song_leads.append(song_lead)
+                        song_lead = copy.deepcopy(song_lead_template)
+                    if operator == '.':
+                        for index in xrange(len(LEAD_TEMPLATE)):
+                            song_lead[index].append('. ')
+
+                else:
+                    add_song_lead(song_lead, notes)
+            elif len(operators) > 1:
                 add_song_lead(song_lead, notes)
-        elif len(operators) > 1:
-            add_song_lead(song_lead, notes)
-        else:
-            print 'Corrupt line: {0}'.format(line)
+            else:
+                print 'Corrupt line: {0}'.format(line)
 
-    if song_lead != song_lead_template:
-        song_leads.append(song_lead)
+        if song_lead != song_lead_template:
+            song_leads.append(song_lead)
 
 
-for lead in song_leads:
-    print_lead(lead)
-
+    for lead in song_leads:
+        print_lead(lead)
